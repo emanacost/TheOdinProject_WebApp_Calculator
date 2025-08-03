9664cd93b55fe9b4118786259afc836b0cea88d7
eacosta <eacosta0411@gmail.com>
2025-08-03 12:38:52 -0400

    Add 4 different functions for the basic arithmetic operations

--- calc.js
+++ calc.js
@@ -1,1 +1,22 @@
 console.log("Connected!");
+
+// Four basic functions for four basic arithmetic operations
+
+function add(a, b = 0) {
+   return a + b;
+};
+
+function subtract(a, b = 0) {
+    return a - b;
+};
+
+function multiply(a, b  = 1) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (b == 0) { 
+        return console.log("Can't divide by zero")
+    };
+    return a / b;
+}
